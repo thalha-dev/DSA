bdd42a5b6c5832ce536b6a0b60d9eb1bde49bfb2
thalha-dev <dev.thalha@gmail.com>
2023-09-29 19:33:28 +0530

    neetcode prob:  Longest Consecutive Sequence

---
+++ neetcode-roadmap/longestConsecutiveSequence.js
@@ -0,0 +1,99 @@
+/**
+ * Brute Force
+ * Greedy - Max Score
+ * Time O (N^3) | Space O(1)
+ * https://leetcode.com/problems/longest-consecutive-sequence/
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = (nums, maxScore = 0) => {
+  for (const num of nums) {
+    /* Time O(N) */
+    let [currNum, score] = [num, 1];
+
+    while (isStreak(nums, currNum + 1)) {
+      /* Time O(N * N) */
+      currNum++;
+      score++;
+    }
+
+    maxScore = Math.max(maxScore, score);
+  }
+
+  return maxScore;
+};
+
+const isStreak = (nums, num) => {
+  for (let i = 0; i < nums.length; i++) {
+    /* Time O(N) */
+    const isEqual = nums[i] === num;
+    if (isEqual) return true;
+  }
+
+  return false;
+};
+
+/**
+ * Sort - HeapSort Space O(1) | QuickSort Space O(log(K))
+ * Greedy - Max Score
+ * Time O (N * log(N)) | Space O(1)
+ * https://leetcode.com/problems/longest-consecutive-sequence/
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = (nums) => {
+  if (!nums.length) return 0;
+
+  nums.sort((a, b) => a - b); /* Time O(N * log(N)) | Space O(1 || log(N)) */
+
+  return search(nums); /* Time O(N) */
+};
+
+const search = (nums) => {
+  let [maxScore, score] = [1, 1];
+
+  for (let i = 1; i < nums.length; i++) {
+    /* Time O(N) */
+    const isPrevDuplicate = nums[i - 1] === nums[i];
+    if (isPrevDuplicate) continue;
+
+    const isStreak = nums[i] === nums[i - 1] + 1;
+    if (isStreak) {
+      score++;
+      continue;
+    }
+
+    maxScore = Math.max(maxScore, score);
+    score = 1;
+  }
+
+  return Math.max(maxScore, score);
+};
+
+/**
+ * Hash Set - Intelligent Sequence
+ * Greedy - Max Score
+ * Time O (N) | Space O(N)
+ * https://leetcode.com/problems/longest-consecutive-sequence/
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = (nums, maxScore = 0) => {
+  const numSet = new Set(nums); /* Time O(N) | Space O(N) */
+
+  for (const num of [...numSet]) {
+    if (numSet.has(num - 1)) continue; /* Time O(N) */
+
+    let [currNum, score] = [num, 1];
+
+    while (numSet.has(currNum + 1)) {
+      /* Time O(N) */
+      currNum++;
+      score++;
+    }
+
+    maxScore = Math.max(maxScore, score);
+  }
+
+  return maxScore;
+};
