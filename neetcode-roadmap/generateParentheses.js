a1ece0f7200a49a30544cf118e215508085da4f1
thalha-dev <dev.thalha@gmail.com>
2023-10-01 07:20:26 +0530

    neetcode prob:  generate parenteses

---
+++ neetcode-roadmap/generateParentheses.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+  const stack = [];
+  const res = [];
+
+  function backtrack(openN, closedN) {
+    if (openN === closedN && openN === n) {
+      res.push(stack.join(""));
+      return;
+    }
+
+    if (openN < n) {
+      stack.push("(");
+      backtrack(openN + 1, closedN);
+      stack.pop();
+    }
+    if (closedN < openN) {
+      stack.push(")");
+      backtrack(openN, closedN + 1);
+      stack.pop();
+    }
+  }
+
+  backtrack(0, 0);
+  return res;
+};
+
+// Example usage:
+const n = 3;
+console.log(generateParenthesis(n));
