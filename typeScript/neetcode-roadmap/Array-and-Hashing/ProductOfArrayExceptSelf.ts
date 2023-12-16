7abcf05a50562e19db665b07afad1ccea109810f
thalha-dev <dev.thalha@gmail.com>
2023-12-16 07:04:32 +0530

    prob: ProductOfArrayExceptSelf.ts

---
+++ typeScript/neetcode-roadmap/Array-and-Hashing/ProductOfArrayExceptSelf.ts
@@ -0,0 +1,20 @@
+function productExceptSelf(nums: number[]): number[] {
+  const array: Array<number> = [];
+  let product: number = 1;
+
+  for (let idx = 0; idx < nums.length; idx++) {
+    array[idx] = product;
+    product *= nums[idx];
+  }
+
+  product = 1;
+
+  for (let idx = nums.length - 1; idx >= 0; idx--) {
+    array[idx] *= product;
+    product *= nums[idx];
+  }
+
+  return array;
+}
+
+console.log(productExceptSelf([1, 2, 3, 4]));
