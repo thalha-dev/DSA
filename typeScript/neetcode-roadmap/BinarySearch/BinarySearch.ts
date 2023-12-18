8d80fd007bdf8a958d8545e4c0d811a9065bb240
thalha-dev <dev.thalha@gmail.com>
2023-12-18 16:13:22 +0530

    prob: BinarySearch.ts

---
+++ typeScript/neetcode-roadmap/BinarySearch/BinarySearch.ts
@@ -0,0 +1,19 @@
+function search(nums: number[], target: number): number {
+  let l = 0,
+    r = nums.length - 1;
+
+  while (l <= r) {
+    var m = Math.round(l + (r - l) / 2);
+    if (nums[m] > target) {
+      r = m - 1;
+    } else if (nums[m] < target) {
+      l = m + 1;
+    } else {
+      return m;
+    }
+  }
+
+  return -1;
+}
+
+export default search;
