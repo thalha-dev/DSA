5c0eacee64aa6cce58dfa544e3f9ffe66b815ad0
thalha-dev <dev.thalha@gmail.com>
2024-04-12 17:47:45 +0530

    NRM - Group Anagrams

--- java/neetcode-roadmap/src/main/java/com/thalha/neetcoderoadmap/NeetcodeRoadmapApplication.java
+++ java/neetcode-roadmap/src/main/java/com/thalha/neetcoderoadmap/NeetcodeRoadmapApplication.java
@@ -3,15 +3,15 @@ package com.thalha.neetcoderoadmap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.thalha.neetcoderoadmap.arrayAndHashing.TwoSum;
+import com.thalha.neetcoderoadmap.arrayAndHashing.GroupAnagrams;
 
 @SpringBootApplication
 public class NeetcodeRoadmapApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(NeetcodeRoadmapApplication.class, args);
-    TwoSum twoSum = new TwoSum();
-    twoSum.run();
+    GroupAnagrams groupAnagrams = new GroupAnagrams();
+    groupAnagrams.run();
   }
 
 }
